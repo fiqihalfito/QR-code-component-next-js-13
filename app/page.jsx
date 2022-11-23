9331ac2104cf6144fe403c5183962ce8d6fd1dc5
fiqihalfito <fiqihalfito.dev@gmail.com>
2022-11-23 23:10:59 +0700

    turn single component to some components

--- app/page.jsx
+++ app/page.jsx
@@ -3,22 +3,38 @@ import Image from 'next/image'
 import styles from './page.module.scss'
 import qrCodeImage from '../public/images/image-qr-code.png'
 
-export default function Home() {
+function CardImage() {
+  return <Image src={qrCodeImage} alt="QR code picture" className={styles.image} />
+}
+
+function CardBody() {
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <div className={styles.cardImage}>
-          <Image src={qrCodeImage} alt="QR code picture" className={styles.image} />
-        </div>
-        <div className={styles.cardBody}>
-          <p className={styles.cardTitle}>
-            Improve your front-end skills by building projects
-          </p>
-          <p className={styles.cardText}>
-            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-          </p>
-        </div>
-      </div>
+    <div className={styles.cardBody}>
+      <p className={styles.cardTitle}>
+        Improve your front-end skills by building project
+      </p>
+      <p className={styles.cardText}>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+      </p>
     </div>
   )
 }
+
+function Card() {
+  return (
+    <div className={styles.card}>
+      <CardImage />
+      <CardBody />
+    </div>
+  )
+}
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Card />
+    </div>
+  )
+}
+
+
